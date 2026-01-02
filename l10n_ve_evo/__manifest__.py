249849d976014d32b63ed39265e579edae447d47
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-02 14:23:06 -0400

    link

--- l10n_ve_evo/__manifest__.py
+++ l10n_ve_evo/__manifest__.py
@@ -1,19 +1,46 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+# Copyright 2024 - l10n_ve_evo
 {
-    'name': 'Venezuela - Accounting (evo)',
-    'website': 'https://www.odoo.com/documentation/latest/applications/finance/fiscal_localizations.html',
+    'name': 'Venezuela - Contabilidad (Evolsys)',
     'countries': ['ve'],
     'author': 'Evolsys',
+    "summary": "Localización Contable para Venezuela",
     'category': 'Accounting/Localizations/Account Charts',
-    'description': """
-Chart of Account for Venezuela.
-===============================
-""",
+    "version": "19.0.2.4.0",
+    'summary': 'Localización Contable para Venezuela (VE) para Odoo 19',
     'depends': [
+        
+        'base',
+        'base_setup',
         'account',
+        'accountant',
+        'account_accountant',
+        'rate_evo',
+        
     ],
-    'auto_install': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/res.country.state.csv',
+        'data/res.country.municipality.csv',
+        'data/res.country.parish.csv',
+        'data/res.city.csv',
+        'data/account.group-ve.xml',
+        'data/account.account-ve.xml',        
+        'data/account.tax.group.xml',
+        'data/account.tax.xml',
+        'data/account.journal.xml',
+        'data/account.fiscal.position.xml',
+        'data/ve_localization_data.xml',
+        'views/res_partner_company_view.xml',
+        'views/res_partner_person_view.xml',
+        
+    ],
+ 
 
-
+   
+    'auto_install': False,
+    'installable': True,
+    'images': ['static/description/icon.png'],
     'license': 'LGPL-3',
+    #'chart_template_id': 'l10n_ve_evo.ve_chart_template',
 }
