cd184541adfc9ba4ea10fbd93d29dac21bb89f49
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-02 14:50:50 -0400

    evo

--- l10n_ve_evo/__manifest__.py
+++ l10n_ve_evo/__manifest__.py
@@ -2,21 +2,19 @@
 # Copyright 2024 - l10n_ve_evo
 {
     'name': 'Venezuela - Contabilidad (Evolsys)',
+    'countries': ['ve'],
     'author': 'Evolsys',
-    'version': "19.0.0.0.4",
+    "summary": "Localización Contable para Venezuela",
     'category': 'Accounting/Localizations/Account Charts',
-    'license': 'LGPL-3',
+    "version": "19.0.2.4.1",
     'summary': 'Localización Contable para Venezuela (VE) para Odoo 19',
     'depends': [
+        
         'base',
         'base_setup',
-        'contacts',
         'account',
+        'accountant',
         'account_accountant',
-        'account_asset',
-        'stock',
-        'sale_management',
-        'purchase',
         'rate_evo',
         
     ],
@@ -27,19 +25,22 @@
         'data/res.country.parish.csv',
         'data/res.city.csv',
         'data/account.group-ve.xml',
-        'data/account.account-ve.xml',
-        'data/account.journal.xml',
+        'data/account.account-ve.xml',        
         'data/account.tax.group.xml',
         'data/account.tax.xml',
+        'data/account.journal.xml',
         'data/account.fiscal.position.xml',
-        #'data/account.chart.template.xml',
+        'data/ve_localization_data.xml',
         'views/res_partner_company_view.xml',
         'views/res_partner_person_view.xml',
+        
     ],
-    'post_init_hook': '_post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
+ 
 
-    'installable': True,
+   
     'auto_install': False,
+    'installable': True,
     'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    #'chart_template_id': 'l10n_ve_evo.ve_chart_template',
 }
--- l10n_ve_evo/models/__init__.py
+++ l10n_ve_evo/models/__init__.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
 from . import res_partner
 from . import res_city
 from . import res_country_municipality
+#from . import paquete
+from . import template_ve 
+
