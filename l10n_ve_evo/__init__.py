67303d95a06f4d85d6e0ce4d07d563dfa2a06ed7
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-06 13:12:58 -0400

    init

--- l10n_ve_evo/__init__.py
+++ l10n_ve_evo/__init__.py
@@ -1,2 +1,7 @@
 from . import models
-from .hooks import create_company_if_missing
+try:
+    from . import hooks as _hooks
+    create_company_if_missing = getattr(_hooks, 'create_company_if_missing', None)
+except Exception:
+    import logging
+    logging.getLogger(__name__).exception('Error importing hooks module')
--- l10n_ve_evo/__manifest__.py
+++ l10n_ve_evo/__manifest__.py
@@ -5,14 +5,13 @@
     'summary': 'Localización Contable para Venezuela (VE) para Odoo 19',
     'category': 'Accounting/Localizations/Account Charts',
     'countries': ['ve'],
-    'version': '19.0.2.5.1',
+    'version': '19.0.2.5.2',
     'depends': [
         'base',
         'base_setup',
         'account',
         'accountant',
         'account_accountant',
-        'account_no_followup',
         'rate_evo',
     ],
     'data': [
@@ -22,10 +21,7 @@
         'data/res.country.parish.csv',
         'data/res.city.csv',
         'data/account.group-ve.xml',
-        # 'data/account.account-ve.xml',  # moved to hook: create accounts in post_init
-        'data/account.tax.group.xml',
-        # 'data/account.tax.xml',        # moved to hook: create taxes & repartition lines in post_init
-        'data/account.journal.xml',
+        'data/account.tax.group.xml','data/account.journal.xml',
         'data/account.fiscal.position.xml',
         'views/res_partner_company_view.xml',
         'views/res_partner_person_view.xml',
