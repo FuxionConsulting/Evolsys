1401721dd88ef18fd1c8f97ecd3d44586944b55b
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-06 11:59:12 -0400

    tools

--- l10n_ve_evo/__manifest__.py
+++ l10n_ve_evo/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Evolsys',
     "summary": "Localización Contable para Venezuela",
     'category': 'Accounting/Localizations/Account Charts',
-    "version": "19.0.2.5.0",
+    "version": "19.0.2.5.1",
     'summary': 'Localización Contable para Venezuela (VE) para Odoo 19',
     'depends': [
         
@@ -24,8 +24,7 @@
         'data/res.country.municipality.csv',
         'data/res.country.parish.csv',
         'data/res.city.csv',
-        'data/account.group-ve.xml',
-        'data/account.account-ve.xml',        
+        'data/account.group-ve.xml',       
         'data/account.tax.group.xml',
         'data/account.tax.xml',
         'data/account.journal.xml',
--- l10n_ve_evo/models/__init__.py
+++ l10n_ve_evo/models/__init__.py
@@ -1,5 +1,5 @@
 from . import res_partner
 from . import res_city
 from . import res_country_municipality
-from . import template_ve 
+
 
