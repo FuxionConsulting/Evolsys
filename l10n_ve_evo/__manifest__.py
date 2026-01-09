5c009c3c2e777410edc5ff09b90be4ecdc77cc0e
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-09 13:31:54 -0400

    7.2

--- l10n_ve_evo/__manifest__.py
+++ l10n_ve_evo/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Localización Contable para Venezuela (VE) para Odoo 19',
     'category': 'Accounting/Localizations/Account Charts',
     'countries': ['ve'],
-    'version': '19.0.2.7.1',
+    'version': '19.0.2.7.2',
     'depends': [
         'base',
         'base_setup',
