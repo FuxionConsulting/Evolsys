b2b03522bba10733684ebb950584524c113aa230
RSEVOLSYS <rafax.suarez@hotmail.com>
2026-01-18 18:50:47 -0400

    DOM1.0

--- l10n_ve_evolsys/__manifest__.py
+++ l10n_ve_evolsys/__manifest__.py
@@ -4,7 +4,7 @@
     "website": "https://www.odoo.com/documentation/17.0/applications/finance/fiscal_localizations.html",
     "icon": "/account/static/description/l10n.png",
     "countries": ["ve"],
-    "author": "Odoo S.A., evolsys C.A",
+    "author": "Evolsys C.A",
     "category": "Accounting/Localizations/Account Charts",
     "description": """
         Chart of Account for Venezuela.
